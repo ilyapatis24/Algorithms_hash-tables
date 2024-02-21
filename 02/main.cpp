b5e5002813d881e5e24830f8985dda2047fc612a
ilyapatis24 <ilyapatis24@mail.ru>
2024-02-21 21:04:51 +0300

    second mission complete

---
+++ 02/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <tgmath.h>
+
+using namespace std;
+
+int realStringHash(string& word, int primeNumber, int module)
+{
+    int hash = 0;
+    for (int i = 0; i < word.length(); ++i)
+    {
+        hash += static_cast<int>(word[i]) * static_cast<int>(pow(primeNumber, i));
+    }
+
+    return hash % module;
+}
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+    int primeNumber = 1;
+    int module = 1;
+    string input = "";
+
+    cout << "Введите p: ";
+    cin >> primeNumber;
+    cout << "Введите n: ";
+    cin >> module;
+
+    while (input != "exit")
+    {
+        cout << "Введите строку: ";
+        cin >> input;
+
+        cout << "Хэш строки " << input << " = " << realStringHash(input, primeNumber, module) << endl;
+    }
+
+    return 0;
+}
