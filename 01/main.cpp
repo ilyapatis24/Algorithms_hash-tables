ea61616d7bdc1ddc25e20466366882a068c42be4
ilyapatis24 <ilyapatis24@mail.ru>
2024-02-21 20:52:40 +0300

    initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int hashString(string& word)
+{
+    int hash = 0;
+    for (int i = 0; i < word.length(); ++i)
+    {
+        hash += static_cast<int>(word[i]);
+    }
+
+    return hash;
+}
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+    string input = "";
+
+    while (input != "exit")
+    {
+        cout << "Введите строку: ";
+        cin >> input;
+
+        cout << "Наивный хэш строки " << input << " = " << hashString(input) << endl;
+    }
+
+    return 0;
+}
